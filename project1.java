0b8d40cb0c0f5ee22d80133452d7f5e85e04c53e
Jonathan Ebert <jebert142@gmail.com>
2016-09-18 14:52:26 -0400

    Initial Push

---
+++ project1.java
@@ -0,0 +1,19 @@
+
+public class project1 {
+
+	public static void main(String[] args) {
+	 
+		System.out.print("Menu \n");
+		System.out.print("[1] Host current Time and Date \n");
+		System.out.print("[2] Host uptime \n");
+		System.out.print("[3] Host Memory use \n");
+		System.out.print("[4] Host Netstat \n");
+		System.out.print("[5] Host current users \n");
+		System.out.print("[6] Host running processes \n");
+		System.out.print("[7] Quit \n");
+		
+	
+
+	}
+
+}

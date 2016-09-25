f9fb60305911980da2775f98508bbc47f76d6943
william-perley <n00636615@ospreys.unf.edu>
2016-09-25 14:59:31 -0400

    Server

---
+++ project1server.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class project1server {
+	
+	int portNum = 1943
+
+}
+
+public void run{
+	//Dispays menu
+String options = "Menu\n0 [1] Host current Time and Date\n1 [2] Host uptime\n3 [3] Host Memory use\n4 "
+		+ "[4] Host Netstat\n5 [5] Host current users\n6 [6] Host running processes\n7 [7]Quit\n8";
+}
